ce2e38faef81dda371b039fe0d3da2e4876dba97
Arun Malik <malikarun86@gmail.com>
2013-11-05 19:28:07 -0800

    changed path in Book Resource

--- library/src/main/java/edu/sjsu/cmpe/library/api/resources/BookResource.java
+++ library/src/main/java/edu/sjsu/cmpe/library/api/resources/BookResource.java
@@ -19,7 +19,7 @@ import edu.sjsu.cmpe.library.ui.views.HomeView;
 
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-@Path("/")
+@Path("/v1/books")
 public class BookResource {
 
 	public BookResource() {
